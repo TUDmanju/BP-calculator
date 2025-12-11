6be73b30e91991f4895d61d041725fe071ecf406
manju <manjushree0301@gmail.com>
2025-12-11 22:22:55 +0000

    Updated UI for new RiskMessage feature

--- BPCalculator/Pages/Index.cshtml.cs
+++ BPCalculator/Pages/Index.cshtml.cs
@@ -1,29 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-// page model
+using BPCalculator;
 
 namespace BPCalculator.Pages
 {
-    public class BloodPressureModel : PageModel
+    public class IndexModel : PageModel
     {
-        [BindProperty]                              // bound on POST
-        public BloodPressure BP { get; set; }
+        [BindProperty]
+        public BloodPressure BloodPressure { get; set; } = new BloodPressure();
 
-        // setup initial data
         public void OnGet()
         {
-            BP = new BloodPressure() { Systolic = 100, Diastolic = 60 };
+            BloodPressure = new BloodPressure
+            {
+                Systolic = 100,
+                Diastolic = 60
+            };
         }
 
-        // POST, validate
         public IActionResult OnPost()
         {
-            // extra validation
-            if (!(BP.Systolic > BP.Diastolic))
+            if (!ModelState.IsValid)
+                return Page();
+
+            if (BloodPressure.Systolic <= BloodPressure.Diastolic)
             {
                 ModelState.AddModelError("", "Systolic must be greater than Diastolic");
+                return Page();
             }
+
             return Page();
         }
     }
